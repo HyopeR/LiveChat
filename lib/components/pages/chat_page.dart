0af0726f09e25f3ca664a5cb438970cffd138fcc
HyopeR <tol_celik@hotmail.com>
2020-11-04 07:12:00 +0300

    chat page updated

--- lib/components/pages/chat_page.dart
+++ lib/components/pages/chat_page.dart
@@ -30,79 +30,124 @@ class _ChatPageState extends State<ChatPage> {
 
     return Scaffold(
         appBar: AppBar(
-          leadingWidth: 85,
-          leading: InkWell(
-            onTap: () => Navigator.of(context).pop(),
-            child: ContainerRow(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Icon(Platform.isAndroid ? Icons.arrow_back : Icons.arrow_back_ios,),
-                ImageWidget(
-                  imageUrl: widget.chatUser.userProfilePhotoUrl,
-                  imageWidth: 50,
-                  imageHeight: 50,
-                  backgroundShape: BoxShape.circle,
-                ),
-              ],
+            leadingWidth: 85,
+            leading: InkWell(
+              onTap: () => Navigator.of(context).pop(),
+              child: ContainerRow(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Icon(
+                    Platform.isAndroid
+                        ? Icons.arrow_back
+                        : Icons.arrow_back_ios,
+                  ),
+                  ImageWidget(
+                    imageUrl: widget.chatUser.userProfilePhotoUrl,
+                    imageWidth: 50,
+                    imageHeight: 50,
+                    backgroundShape: BoxShape.circle,
+                  ),
+                ],
+              ),
             ),
-          ),
-
-          title: Text(widget.chatUser.userName)
-        ),
+            title: Text(widget.chatUser.userName)),
         body: SafeArea(
           child: ContainerColumn(
             padding: EdgeInsets.all(10),
             children: [
               Expanded(
-                child: StreamBuilder<List<ChatModel>>(
-                  stream: _chatView.getMessages(widget.currentUser.userId, widget.chatUser.userId),
-                  builder: (context, streamData){
+                  child: StreamBuilder<List<ChatModel>>(
+                stream: _chatView.getMessages(
+                    widget.currentUser.userId, widget.chatUser.userId),
+                builder: (context, streamData) {
+                  List<ChatModel> messages = streamData.data;
 
-                    List<ChatModel> messages = streamData.data;
+                  print((messages != null).toString());
 
-                    if(messages != null) {
-                      if(messages.isNotEmpty)
-                        return ListView.builder(
-                            itemCount: messages.length,
-                            itemBuilder: (context, index) {
-                              return Text(messages[index].message);
-                            }
-                        );
-
-                      else
-                        return Center(child: Text('Bir konuşma başlat'));
-                    } else
-                      return Container();
-
-                  },
-                )
-              ),
-
+                  if (messages != null) {
+                    if (messages.isNotEmpty)
+                      return ListView.builder(
+                          itemCount: messages.length,
+                          itemBuilder: (context, index) {
+                            return createTalkingBalloon(messages[index]);
+                          });
+                    else
+                      return Center(child: Text('Bir konuşma başlat'));
+                  } else
+                    return Container();
+                },
+              )),
               MessageCreatorWidget(
                 key: _messageCreatorState,
                 hintText: 'Bir mesaj yazın.',
-                textAreaColor: Colors.grey.withOpacity(0.3),
+                textAreaColor: Colors.grey.shade300.withOpacity(0.8),
                 buttonColor: Theme.of(context).primaryColor,
-                onPressed: () async {
-
-                  if(_messageCreatorState.currentState.controller.text.trim().length > 0) {
-                    ChatModel savingMessage = ChatModel(
-                      senderId: widget.currentUser.userId,
-                      receiverId: widget.chatUser.userId,
-                      fromMe: true,
-                      message: _messageCreatorState.currentState.controller.text,
-                    );
-
-                    bool result = await _chatView.saveMessage(savingMessage);
-                    if(result)
-                      _messageCreatorState.currentState.controller.clear();
-                  }
-
-                },
+                onPressed: () => saveMessage(),
               )
             ],
           ),
         ));
+  }
+
+  void saveMessage() async {
+    if (_messageCreatorState.currentState.controller.text.trim().length > 0) {
+      ChatModel savingMessage = ChatModel(
+        senderId: widget.currentUser.userId,
+        receiverId: widget.chatUser.userId,
+        fromMe: true,
+        message: _messageCreatorState.currentState.controller.text,
+      );
+
+      bool result = await _chatView.saveMessage(savingMessage);
+      if (result) _messageCreatorState.currentState.controller.clear();
+    }
+  }
+
+  Widget createTalkingBalloon(ChatModel message) {
+    Color _outgoingMessageColor = Theme.of(context).primaryColor.withOpacity(0.8);
+    Color _incomingMessageColor = Colors.grey.shade300.withOpacity(0.8);
+    bool _fromMe = message.fromMe;
+
+    if (_fromMe)
+      return ContainerColumn(
+        margin: EdgeInsets.only(top: 5),
+        alignment: Alignment.centerRight,
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(10),
+            onTap: () {},
+            child: Material(
+              elevation: 0,
+              borderRadius: BorderRadius.circular(10),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(message.message),
+              ),
+              color: _outgoingMessageColor,
+            ),
+          ),
+        ],
+      );
+    else
+      return ContainerColumn(
+        margin: EdgeInsets.only(top: 5),
+        alignment: Alignment.centerLeft,
+        children: [
+          InkWell(
+            borderRadius: BorderRadius.circular(10),
+            onTap: () {},
+            child: Material(
+              elevation: 0,
+              borderRadius: BorderRadius.circular(10),
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(message.message),
+              ),
+              color: _incomingMessageColor,
+            ),
+          ),
+        ],
+      );
   }
 }
